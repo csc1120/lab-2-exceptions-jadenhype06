0e6eaf3418eb12b5119b796d8ecb5c718aa51eef
hipej <hipej@msoe.edu>
2024-09-17 22:42:10 -0500

    Wrote class and Output is not exactly correct format

--- src/hipej/Driver.java
+++ src/hipej/Driver.java
@@ -9,27 +9,46 @@ package hipej;
 
 import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
-
+/**
+ * Driver class
+ */
 public class Driver {
+    /**
+     * Minimum dice
+     */
     public static int MIN_DICE = 2;
+    /**
+     * Max dice
+     */
     public static int MAX_DICE = 10;
     public static void main(String[] args) {
         int[] list;
         list = getInput();
-        int[] rolled = rollDice(createDice(list[0],list[1]),list[1],list[2]);
+        int[] rolled = rollDice(createDice(list[0], list[1]), list[1], list[2]);
         int max = findMax(rolled);
         report(list[0], rolled, max);
 
     }
+
+    /**
+     * Finds highest number of values rolled,
+     * @param rolls array of rolls
+     * @return most common value
+     */
     public static int findMax(int[] rolls) {
         int max = 0;
         for (int roll : rolls) {
-            if (roll > max)
+            if (roll > max) {
                 max = roll;
+            }
         }
         return max;
     }
+
+    /**
+     * gets input from user
+     * @return split String into integers
+     */
     public static int[] getInput() {
         boolean validinput = false;
         int[] y = new int[3];
@@ -63,13 +82,27 @@ public class Driver {
         return y;
     }
 
-    public static Die[] createDice (int numDice, int numSides) {
+    /**
+     * initializes the dice and puts them in a list
+     * @param numDice number of dice
+     * @param numSides number of sides on dice
+     * @return array of dice
+     */
+    public static Die[] createDice(int numDice, int numSides) {
         Die[] Dice = new Die[numDice];
         for (int i = 0; i < Dice.length; i++) {
             Dice[i] = new Die(numSides);
         }
         return Dice;
     }
+
+    /**
+     * Rolls the dice
+     * @param dice array of dice
+     * @param numSides number of sides on dice
+     * @param numRolls number of rolls that will occur
+     * @return array of stats
+     */
     public static int[] rollDice(Die[] dice, int numSides, int numRolls) {
         int[] all = new int[numRolls];
         int[] possible = new int[(dice.length * numSides) - dice.length + 1];
@@ -101,22 +134,27 @@ public class Driver {
     }
 
 
-
-        public static void report (int numDice, int [] rolls, int max) {
+    /**
+     * graphs the dice stats
+     * @param numDice number of dice
+     * @param rolls array of stats of rolled dice
+     * @param max highest stat
+     */
+    public static void report(int numDice, int[] rolls, int max) {
         int scale = max / 10;
         int count = 0;
-            for (int roll : rolls) {
-                int numStars = roll / scale;
-                String stars = "";
+        for (int roll : rolls) {
+            int numStars = roll / scale;
+            String stars = "";
 
-                for (int j = 0; j < numStars; j++) {
-                    stars += "*";
-                }
-
-                int data = numDice + count;
-                count++;
-                System.out.printf("%d :"+ roll +" \t%s", data, stars);
-                System.out.println();
+            for (int j = 0; j < numStars; j++) {
+                stars += "*";
             }
+
+            int data = numDice + count;
+            count++;
+            System.out.printf("%d :"+ roll +" \t%s", data, stars);
+            System.out.println();
+        }
     }
 }
